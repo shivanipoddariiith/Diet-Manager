662489c223af1d4bf887e4d879e3c94c736f1f5d
Mahesh Attarde <coder.mahesh@gmail.com>
2014-03-17 18:51:39 +0530

    UserProfile and DietManager Algorithms

--- UI_StrategySkeleton/ClrTest/ClrTest/CalorieCalculation.cpp
+++ UI_StrategySkeleton/ClrTest/ClrTest/CalorieCalculation.cpp
@@ -1,10 +1,11 @@
 
+#include "UserProfile.h"
 
-
+ 
 class IDietCalculator
 {	
 public:
-	virtual int getTargetCalorieIntake()=0;
+	virtual int getTargetCalorieIntake(UserProfile user)=0;
 };
 
 class BMRBasedDietCalculator :public IDietCalculator
@@ -12,9 +13,17 @@ class BMRBasedDietCalculator :public IDietCalculator
 
 	/*virtual*/
 	
-	int getTargetCalorieIntake()
+	int getTargetCalorieIntake(UserProfile user)
 	{
-		return 99;
+		// Harris-Benedict equation for BMR
+		if (user.getGender == true)
+		{
+			return (13.75 * user.getWeight()) + (5 * user.getHeight()) - (6.76 * user.getActivityLevel) + 66;
+		}
+		else
+		{
+			return (9.57 * user.getWeight()) + (1.85 * user.getHeight()) - (4.68 * user.getActivityLevel) +655;
+		}
 	}
 
 };
@@ -24,9 +33,18 @@ class RMRBasedDietCalculator :public IDietCalculator
 
 	/*virtual*/
 
-	int getTargetCalorieIntake()
+	int getTargetCalorieIntake(UserProfile user)
 	{
-		return 999;
+		//Mufflin equation for RMR
+		if (user.getGender == true)
+		{
+			return (10 * user.getWeight()) + (6.25 * user.getHeight()) - (5* user.getActivityLevel) + 5;
+		}
+		else
+		{
+			return (10 * user.getWeight()) + (6.25 * user.getHeight()) - (5 * user.getActivityLevel) - 161;
+		}
+
 	}
 
 };
